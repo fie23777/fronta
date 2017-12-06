2a6402b15e5f1a50302107f0361784c7e21a7370
dayane.sandi <dayane.sandi@hotmail.com>
2017-12-06 14:17:35 -0200

    BootStrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,22 @@ import { NgModule } from '@angular/core';
 import {HttpModule} from '@angular/http';
 import {FormsModule} from '@angular/forms';
 import {ClienteService} from './server/cliente.service';
-
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
 import { AppComponent } from './app.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,HttpModule,FormsModule
+    BrowserModule,HttpModule,FormsModule, BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot()
   ],
   providers: [ClienteService],
   bootstrap: [AppComponent]
